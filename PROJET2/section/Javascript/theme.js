f50ff305b100e6d5758ec07eff67dfd1d0f536d3
Riyaneb <riyane.bouakaz@etu.cyu.fr>
2026-04-30 04:42:12 +0200

    Ajout du js pour gerer le theme et des cookies

--- PROJET2/section/Javascript/theme.js
+++ PROJET2/section/Javascript/theme.js
@@ -6,9 +6,46 @@ function changerTheme(){
     if(nomCSS.includes("Variable.css")){
         bouton.innerHTML = "Mode sombre 🌙";
         link.setAttribute("href","CSS/VariableClair.css");
+        enregistreCookie("theme","clair");
     }
     else if(nomCSS.includes("VariableClair.css")){
         bouton.innerHTML = "Mode clair ☀️";
         link.setAttribute("href","CSS/Variable.css");
+        enregistreCookie("theme","sombre");
     }
 }
+
+function enregistreCookie(nom,valeur){
+    document.cookie = nom + "=" + valeur + "; path=/;";
+}
+
+function lireCookie(nom,origine){
+    let tab = document.cookie.split("; ");
+    for(let e of tab){
+        let [cle,valeur] = e.split("=");
+        if(cle == nom){
+            return valeur;
+        }
+    }
+    return origine;
+}
+
+function themeChargement(){
+    let theme = lireCookie("theme","sombre");
+    let link = document.getElementById("theme");
+    let nomCSS = link.getAttribute("href");
+    let bouton = document.getElementById("bouttonTheme");
+    if(theme == "clair"){
+        bouton.innerHTML = "Mode sombre 🌙";
+        link.setAttribute("href","CSS/VariableClair.css");
+    }
+    else if(theme == "sombre"){
+        bouton.innerHTML = "Mode clair ☀️";
+        link.setAttribute("href","CSS/Variable.css");
+    }
+    else{
+        console.log("Erreur avec le cookie du thème");
+    }
+}
+
+themeChargement();
